74494425b6844b77d0b81286c743036880f1cd32
_S1t_ <59321990+reter229@users.noreply.github.com>
2022-12-11 12:40:11 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,31 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    "use strict";
+    window.addEventListener("load", (() => {
+        document.documentElement.classList.add("_loaded");
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
